9b506865a3345ef754264c7bc2451ed8c7bf0d1c
Benjamin Nathan Peloquin <bpeloqui@corn24.stanford.edu>
2015-01-28 20:29:28 -0800

    FL death row scriptp - combine and parse three web pages for race/ethnicity

---
+++ homework/death-row-parsing/fl-dr.sh
@@ -0,0 +1,11 @@
+###----------------fl-dr.sh----------------###
+p="data-hold/"
+cat $p\fl1.html $p\fl2.html $p\fl3.html | pup 'td:nth-of-type(3) text{}' 
+
+#Code using a for-loop saving aggregate data into a new file
+#touch fl-aggregate.txt
+#for page in $(seq 3)
+#do
+#cat data-hold/fl$page.html | pup 'td:nth-of-type(3) text{}' >> fl-aggregate.txt
+#done
+
