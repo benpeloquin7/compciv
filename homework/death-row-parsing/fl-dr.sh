85e6a3d5d74a3a2cf7484adecdc3257da28c1439
Benjamin Nathan Peloquin <bpeloqui@corn24.stanford.edu>
2015-01-28 22:26:52 -0800

    fixed spacing bugs, removes race/gender headers

--- homework/death-row-parsing/fl-dr.sh
+++ homework/death-row-parsing/fl-dr.sh
@@ -1,7 +1,11 @@
 ###----------------fl-dr.sh----------------###
+touch fl-tmp.txt
 p="data-hold/"
 cat $p\fl1.html $p\fl2.html $p\fl3.html | pup 'td:nth-of-type(3) text{}'\
-| grep -vP '\xc2\xa0'
+| grep -vP '\xc2\xa0' | grep -v 'Race' >> fl-tmp.txt
+
+#Output
+cat fl-tmp.txt
 
 #Code using a for-loop saving aggregate data into a new file
 #touch fl-aggregate.txt
