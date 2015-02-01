5ade6b44b45a3bb9e5528825ca54ce6a0eef398c
Benjamin Nathan Peloquin <bpeloqui@corn05.stanford.edu>
2015-01-31 18:17:56 -0800

    Completed classifier script that takes in name args

--- homework/baby-name-gender-detector/classifier.sh
+++ homework/baby-name-gender-detector/classifier.sh
@@ -4,41 +4,49 @@
 #to create file below
 datafile='data-hold/namesample.txt'
 
-name=Jessie
-
-#Match every line with $name
-name_matches=$(cat $datafile | grep "$name,")
-
-#Gender count vars
-m_count=0
-f_count=0
-
-for row in $name_matches; do
-  #Save the baby count 
-  babies=$(echo $row | cut -d ',' -f 3)
-
-  #Check for gender and add to appropriate var
-  if [[ $row =~ ',M,' ]]; then
-    m_count=$((m_count + babies))
-  else
-    f_count=$((f_count + babies))
-  fi
-done
-
-#Total all babies with name $name
-total_babies=$((m_count + f_count))
-
-#Check that we have at least one baby with $name
-if [[ $total_babies -eq 0 ]]; then
-  g_and_pct="NA,0"
+#First check that user has passed an argument
+if [[ -z $1 ]]; then
+  echo "Please pass in at least one name..."
+#If we have at least one argument 
 else
-  pct_female=$((100 * f_count / total_babies))
-  
-  if [[ pct_female -ge 50 ]]; then
-    g_and_pct="F,$pct_female"
-  else
-   g_and_pct="M,$((100 - $pct_female))"
-  fi
-fi
+  #Loop through all name arguments passed
+  for name in "$@"; do
+    #Match every line with $name
+    name_matches=$(cat $datafile | grep "$name,")
 
-echo "$name,$g_and_pct,$total_babies"
+    #Gender count vars
+    m_count=0
+    f_count=0
+
+    for row in $name_matches; do
+      #Save the baby count 
+      babies=$(echo $row | cut -d ',' -f 3)
+
+      #Check for gender and add to appropriate var
+      if [[ $row =~ ',M,' ]]; then
+        m_count=$((m_count + babies))
+      else
+        f_count=$((f_count + babies))
+      fi
+    done
+
+    #Total all babies with name $name
+    total_babies=$((m_count + f_count))
+
+    #Check that we have at least one baby with $name
+    if [[ $total_babies -eq 0 ]]; then
+      g_and_pct="NA,0"
+    else
+      pct_female=$((100 * f_count / total_babies))
+      
+      if [[ pct_female -ge 50 ]]; then
+        g_and_pct="F,$pct_female"
+      else
+       g_and_pct="M,$((100 - $pct_female))"
+      fi
+    fi
+
+    #Output
+    echo "$name,$g_and_pct,$total_babies"
+  done
+fi
