1eb3d469b8325255a3f522937153888b21e876c6
Benjamin Nathan Peloquin <bpeloqui@corn05.stanford.edu>
2015-01-31 16:56:52 -0800

    extra script to run basic-ifelse.sh with Dan's examples

---
+++ homework/if-else-practice/run-basic-ifelse.sh
@@ -0,0 +1,29 @@
+###:--------run-basic-ifelse.sh----------:#
+#Running basic-ifelse.sh with Dan input
+
+bash basic-ifelse.sh 
+
+bash basic-ifelse.sh 100 200 300 400
+
+bash basic-ifelse.sh 100 200 300 400 500 
+
+bash basic-ifelse.sh 9
+
+bash basic-ifelse.sh 9 9
+
+bash basic-ifelse.sh 9 10
+
+bash basic-ifelse.sh 14 6
+
+bash basic-ifelse.sh 9 9 9
+
+bash basic-ifelse.sh 12 9 9
+
+bash basic-ifelse.sh 12 12 40
+
+bash basic-ifelse.sh 12 12 40
+
+bash basic-ifelse.sh 4 12 40
+
+bash basic-ifelse.sh 95 2 3
+
