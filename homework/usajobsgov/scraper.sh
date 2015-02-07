e14e8f26b4734a29cda3eb680d1383485ae5db3d
Benjamin Nathan Peloquin <bpeloqui@corn26.stanford.edu>
2015-02-06 21:58:41 -0800

    near final version of scraper - appears to pull jobfamilies and associated pages using nested loops

--- homework/usajobsgov/scraper.sh
+++ homework/usajobsgov/scraper.sh
@@ -14,7 +14,27 @@ if [[ ! -d "scraper" ]]; then
 mkdir scraper
 fi
 
-#Create new time-steamp dir in scraper
+#Create new time-steamp dir in scraper and navigate inside
 cd scraper
 currTime=$(bash ../../time.sh)
 mkdir $currTime
+cd $currTime
+
+#Save a file with all job families
+all_Job_Families=$(cat ../../OccupationalSeries.xml |\
+hxselect -c 'JobFamily' -s ',' | sed s/,/\\n/g)
+
+#Curl down all pages
+path=https://data.usajobs.gov/api/jobs?
+
+#Outer loop through all job families
+for jobfam in $all_Job_Families; do
+#curl page number
+pages=$(curl $path"series="$jobfam | jq '. | .Pages')
+#remove whatever non-int char exists there
+pages=$(echo $pages | sed 's/[^0-9]*//g')
+#inner loop through pages
+for nPage in $(seq 1 $pages); do
+curl -o $jobfam"-"$nPage".json" $path"series="$jobfam"&Page="$nPage
+done
+done
