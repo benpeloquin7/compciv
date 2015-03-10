5fe7bc9df57221ba01e7845986092eda3696dd5b
Benjamin Nathan Peloquin <bpeloqui@corn36.stanford.edu>
2015-03-10 08:30:16 -0700

    updated name data parse

--- homework/dallas-ois/officers-maker.sh
+++ homework/dallas-ois/officers-maker.sh
@@ -18,7 +18,7 @@ while read -r line; do
 
         suspect_weapon=$(echo $line | cut -d "|" -f 5)
 
-        echo $line | cut -d "|" -f 6 > officer_list.txt
+        echo $line | cut -d "|" -f 7 | grep -oE "[A-Z][a-z]+, [A-Z][a-z]+ [A-Z]/[A-Z]\b" > officer_list.txt
         while read -r officer; do
                 race=$(echo $officer | grep -oE "[A-Z]/[A-Z]" | cut -d "/" -f 1)
                 gender=$(echo $officer | grep -oE "[A-Z]/[A-Z]" | cut -d "/" -f 2)
