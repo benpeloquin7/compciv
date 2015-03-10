4e809337bb0a2af8e7b2eeefbeea34488083434a
Benjamin Nathan Peloquin <bpeloqui@corn36.stanford.edu>
2015-03-10 08:04:24 -0700

    officers-maker helper script used in table-maker.sh -- uses data from incidents.psv to create officers.psv

---
+++ homework/dallas-ois/officers-maker.sh
@@ -0,0 +1,32 @@
+#####::::-------officers-maker.sh----------:::::#####
+
+
+if [[ ! -e tables/incidents.psv ]]; then
+echo "Error: no tables/incidents.psv file - re-run table-maker.sh"
+elif [[ -e tables/officers.psv ]]; then
+echo "Already have tables/officers.psv file - no need to run this script"
+else
+while read -r line; do
+        case_number=$(echo $line | cut -d "|" -f 1)
+        date=$(echo $line | cut -d "|" -f 2)
+        suspect_killed=$(echo $line | cut -d "|" -f 4)
+        if [[ $suspect_killed == "Deceased" ]]; then
+                suspect_killed="TRUE"
+        else
+                suspect_killed="FALSE"
+        fi
+
+        suspect_weapon=$(echo $line | cut -d "|" -f 5)
+
+        echo $line | cut -d "|" -f 6 > officer_list.txt
+        while read -r officer; do
+                race=$(echo $officer | grep -oE "[A-Z]/[A-Z]" | cut -d "/" -f 1)
+                gender=$(echo $officer | grep -oE "[A-Z]/[A-Z]" | cut -d "/" -f 2)
+                first_name=$(echo $officer | cut -d "," -f 1)
+                last_name=$(echo $officer | cut -d "," -f 2 | grep -oE "[A-Z][a-z]+")
+                printf '%s|%s|%s|%s|%s|%s|%s|%s\n' "$case_number" "$date" "$suspect_killed" "$suspect_weapon" "$last_name" "$first_name" "$race" "$gender" >> tables/officers.psv
+        done < officer_list.txt
+        rm officer_list.txt
+done < tables/incidents.psv
+fi
+
