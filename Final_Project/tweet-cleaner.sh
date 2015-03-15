69c1fee1ff3f35aa6e20d4b0aa20ea2d6eec641d
Benjamin Nathan Peloquin <bpeloqui@corn36.stanford.edu>
2015-03-14 17:19:40 -0700

    tweet-cleaner completed script called by tweet-as.sh

--- Final_Project/tweet-cleaner.sh
+++ Final_Project/tweet-cleaner.sh
@@ -1,15 +1,17 @@
-###:---------tweet-cleaner.sh-----------:###
+###:---------tweet-cleaner2.sh-----------:###
 #Removes http links, twitter handles, new lines and back slashes
 
-path="data-hold/account-store/"
-tweets=$1
+#save file-path and account information
+path="cached-accounts/"
+account=$1
 
-if [[ -e "cleantest.txt" ]]; then
-rm cleantest.txt
+#rm past cleaned tweets
+if [[ -e "$path\current.txt" ]]; then
+rm current.txt
 fi
 
-csvfix order -f 4 $path$tweets | grep -v "RT" |\
+#create new cleaned text file --> current.txt for n-grams.py
+csvfix order -f 4 $path$account.csv | grep -v "RT" |\
 sed -E 's/http.+\b//g' | sed -E 's/@.+\b//g' |\
 tr -d '\"' | tr [A-Z] [a-z] | tr '  +' ' ' |\
-iconv -c -f utf-8 -t ascii > cleantest.txt
-
+iconv -c -f utf-8 -t ascii > current.txt
