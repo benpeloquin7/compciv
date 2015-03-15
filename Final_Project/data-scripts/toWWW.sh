813a30f3d10c5fdb32ceeba249b8a7800c1bf5f0
Benjamin Nathan Peloquin <bpeloqui@corn36.stanford.edu>
2015-03-14 17:26:59 -0700

    removing deleted files

--- Final_Project/data-scripts/toWWW.sh
+++
@@ -1,6 +0,0 @@
-file_to_move=$1
-if [[ -e $file_to_move ]]; then
-mv $file_to_move ~/WWW/$file_to_move
-else
-echo "Error: No file named $file_to_move"
-fi
--- Final_Project/main.sh
+++
@@ -1,4 +0,0 @@
-###:------------main.sh-----------:###
-output=$(python n-grams.py)
-
-echo $output | sed 's/[\.!?]\s*./\U&\E/g'
